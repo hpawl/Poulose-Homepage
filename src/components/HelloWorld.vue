20f4731d7e7a4e68e3d24d6ac42cc24209b824bc
Hasvid <swiffy557@outlook.com>
2025-03-03 16:47:16 -0500

    first commit

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+    import { ref } from 'vue';
+    defineProps<{ msg: string }>()
+    const lang = ref("Vue")
+</script>
+
+<template>
+    <h1>{{ msg }}</h1>
+    <p>This is my first website in {{ lang }}!!</p>
+</template>
+
+<style>
+</style>
